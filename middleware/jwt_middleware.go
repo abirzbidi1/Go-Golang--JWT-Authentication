07020776af74f9947102f38557c7eb6a6a15bf46
Abir Zbidi <124668306+abirzbidi1@users.noreply.github.com>
2025-03-21 10:31:24 +0100

    Update jwt_middleware.go
    Create JWT middleware

---
+++ middleware/jwt_middleware.go
@@ -0,0 +1,36 @@
+package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+var secretKey = "supersecretkey"
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenString := c.GetHeader("Authorization")
+		if tokenString == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Token manquant"})
+			c.Abort()
+			return
+		}
+
+		tokenString = strings.TrimPrefix(tokenString, "Bearer ")
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return []byte(secretKey), nil
+		})
+
+		if err != nil || !token.Valid {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Token invalide"})
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
+}
