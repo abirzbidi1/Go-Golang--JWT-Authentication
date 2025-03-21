319434e5b501f7d39dabec23c9e35f516af8e1f3
Abir Zbidi <124668306+abirzbidi1@users.noreply.github.com>
2025-03-21 10:34:27 +0100

    Update auth_controller.go
    Authentication API

---
+++ controllers/auth_controller.go
@@ -0,0 +1,66 @@
+package controllers
+
+import (
+	"go-jwt-api/models"
+	"net/http"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var secretKey = "supersecretkey"
+
+func Register(c *gin.Context) {
+	var input struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(input.Password), bcrypt.DefaultCost)
+
+	err := models.CreateUser(input.Username, string(hashedPassword))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erreur lors de l'inscription"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "Utilisateur créé"})
+}
+
+func Login(c *gin.Context) {
+	var input struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user, err := models.GetUserByUsername(input.Username)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Utilisateur non trouvé"})
+		return
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(input.Password)); err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Mot de passe incorrect"})
+		return
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": user.ID,
+		"exp":     time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	tokenString, _ := token.SignedString([]byte(secretKey))
+	c.JSON(http.StatusOK, gin.H{"token": tokenString})
+}
